c5a48f932c50d66c66f8be0f54d9d87721d153fd
Achsah Abraham <achsahmp@gmail.com>
2018-10-14 00:12:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,9 @@ class Application
    req = Rack::Request.new(route)
 
    if req.path.match(/items/)
-     item_name = req.path.split("/items/").last
+     fruit = req.path.split("/items/").last
 
-     item = @@items.find{|i| i.name == item_name}
+     item = @@items.find{|i| i.name == fruit}
 
      if item.nil?
        resp.write "Item not found"
