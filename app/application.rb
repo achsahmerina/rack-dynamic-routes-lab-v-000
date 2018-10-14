1a9473a0c4d10bbdb335eb41f077516e52bbb313
Achsah Abraham <achsahmp@gmail.com>
2018-10-14 00:15:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       fruit = req.path.split("/items/").last
 
-      item = @@items.find{|i| i.name == fruit}
+      item = @@items.find{|fruit_price| fruit_price.name == fruit}
 
       if item.nil?
         resp.write "Item not found"
