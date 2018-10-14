c133940ea9950fa6cdfffffb130e282027c566ff
Achsah Abraham <achsahmp@gmail.com>
2018-10-14 00:13:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
        resp.write "Item not found"
        resp.status = 400
      else
-       resp.write item.price
+       resp.write list.price
      end
    else
      resp.write "Route not found"
